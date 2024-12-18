72a0ee4cd5f8133269fc359920705b2b6db4d5c0
Anthony <brayan.farronan@gmail.com>
2024-12-18 15:54:59 +0100

    Commit 001: First commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from './components/navbar/Navbar'
+import './components/navbar/navbar.css'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Navbar />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar(){
+    return (
+        <>
+        <h1 className="titleNavbar">Este es el Navbar</h1>
+        </>
+    )
+}
+
+export default Navbar
